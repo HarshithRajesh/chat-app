2dc87ec60e845af4bf91aeab913979b7f0dc6b3b
Harshith <harshithr2004@gmail.com>
2025-03-25 23:54:25 +0530

    repository file

--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -14,3 +14,19 @@ type userRepository struct {
   db *gorm.DB
 }
 
+func NewUserRepository*db *gorm.DB UserRepository{
+  return &userRepository{db}
+}
+
+func (r *userRepository) CreateUser(user *domain.User) error {
+  return r.db.Create(user).Error 
+}
+
+func (r *userRepository) GetUser(email string)(*domain.User,error){
+  var user domain.User 
+  err := r.db.Where('email =?',email).First(&user).Error 
+  if err != nil {
+    return nil,err 
+  } 
+  return &user,nil 
+}
