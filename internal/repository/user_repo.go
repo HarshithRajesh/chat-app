c2ea0b1673aab66360ea05c9d945b7e1f933293e
Harshith <harshithr2004@gmail.com>
2025-03-29 19:00:11 +0530

    commented out for relearnng

--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -1,32 +1,32 @@
-package repository
-
-import (
-  "github.com/HarshithRajesh/chat-app/internal/domain"
-  "gorm.io/gorm"
-)
-
-type UserRepository interface {
-  CreateUse(user *domain.User) error
-  GetUser(email string)(*domain.User,error)
-}
-
-type userRepository struct {
-  db *gorm.DB
-}
-
-func NewUserRepository*db *gorm.DB UserRepository{
-  return &userRepository{db}
-}
-
-func (r *userRepository) CreateUser(user *domain.User) error {
-  return r.db.Create(user).Error 
-}
-
-func (r *userRepository) GetUser(email string)(*domain.User,error){
-  var user domain.User 
-  err := r.db.Where('email =?',email).First(&user).Error 
-  if err != nil {
-    return nil,err 
-  } 
-  return &user,nil 
-}
+// package repository
+//
+// import (
+//   "github.com/HarshithRajesh/chat-app/internal/domain"
+//   "gorm.io/gorm"
+// )
+//
+// type UserRepository interface {
+//   CreateUse(user *domain.User) error
+//   GetUser(email string)(*domain.User,error)
+// }
+//
+// type userRepository struct {
+//   db *gorm.DB
+// }
+//
+// func NewUserRepository*db *gorm.DB UserRepository{
+//   return &userRepository{db}
+// }
+//
+// func (r *userRepository) CreateUser(user *domain.User) error {
+//   return r.db.Create(user).Error 
+// }
+//
+// func (r *userRepository) GetUser(email string)(*domain.User,error){
+//   var user domain.User 
+//   err := r.db.Where('email =?',email).First(&user).Error 
+//   if err != nil {
+//     return nil,err 
+//   } 
+//   return &user,nil 
+// }
--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -1,41 +1,41 @@
-package service
-
-import (
-  "errors"
-  "github.com/HarshithRajesh/chat-app/internal/domain"
-  "github.com/HarshithRajesh/chat-app/internal/repository"
-  "github.com/HarshithRajesh/chat-app/internal/common"
-)
-
-type UserService interface{
-  Signup(user *domain.User) error 
-  Login(email, password string)(string,error)
-}
-
-type userService struct{
-  repo repository.UserRepository
-}
-
-func NewUserService(repo repository.UserRepository) UserService{
-  return &userService{repo}
-}
-
-func (s *userService) Signup(user *domain.User) error {
-	hashedPassword, err := common.HashPassword(user.Password)
-	if err != nil {
-		return err
-	}
-	user.Password = hashedPassword
-	return s.repo.CreateUser(user)
-}
-
-func (s *userService) Login(email, password string) (string, error) {
-	user, err := s.repository.GetUserByEmail(email)
-	if err != nil {
-		return "", errors.New("invalid credentials")
-	}
-	if !common.CheckPasswordHash(password, user.Password) {
-		return "", errors.New("invalid credentials")
-	}
-	return common.GenerateJWT(user.ID, user.Email)
-}
+// package service
+//
+// import (
+//   "errors"
+//   "github.com/HarshithRajesh/chat-app/internal/domain"
+//   "github.com/HarshithRajesh/chat-app/internal/repository"
+//   "github.com/HarshithRajesh/chat-app/internal/common"
+// )
+//
+// type UserService interface{
+//   Signup(user *domain.User) error 
+//   Login(email, password string)(string,error)
+// }
+//
+// type userService struct{
+//   repo repository.UserRepository
+// }
+//
+// func NewUserService(repo repository.UserRepository) UserService{
+//   return &userService{repo}
+// }
+//
+// func (s *userService) Signup(user *domain.User) error {
+// 	hashedPassword, err := common.HashPassword(user.Password)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	user.Password = hashedPassword
+// 	return s.repo.CreateUser(user)
+// }
+//
+// func (s *userService) Login(email, password string) (string, error) {
+// 	user, err := s.repository.GetUserByEmail(email)
+// 	if err != nil {
+// 		return "", errors.New("invalid credentials")
+// 	}
+// 	if !common.CheckPasswordHash(password, user.Password) {
+// 		return "", errors.New("invalid credentials")
+// 	}
+// 	return common.GenerateJWT(user.ID, user.Email)
+// }
